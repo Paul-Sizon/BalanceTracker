f42c25dc8e933b53b147fbaa86d999ab80f245b6
psizonenko <psizonenko@griddynamics.com>
2022-05-09 22:46:55 +0300

    starting ui

--- app/src/main/java/com/balance/tracker/ui/MainComponents.kt
+++ app/src/main/java/com/balance/tracker/ui/MainComponents.kt
@@ -1,23 +1,33 @@
 package com.balance.tracker.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.balance.tracker.ui.theme.TrackerTheme
 
 @Composable
 fun Balance(balance: Int) {
-    Text(text = balance.toString())
+    Text(text = balance.toString(), Modifier.size(200.dp))
 }
 
 @Composable
 fun Fab() {
-    FloatingActionButton(onClick = { /*do something*/ }) {
-        Icon(Icons.Filled.Favorite, contentDescription = "")
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { /*do something*/ }) {
+            Icon(Icons.Filled.Add, contentDescription = "")
+        }
+    }) {
+        Balance(20)
     }
 }
 
@@ -25,7 +35,6 @@ fun Fab() {
 @Composable
 fun DefaultPreview() {
     TrackerTheme {
-        Balance(20)
         Fab()
     }
 }
