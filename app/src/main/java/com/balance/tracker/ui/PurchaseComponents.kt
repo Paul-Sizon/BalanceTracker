01cb3073ea0293ef60601e83d544fa0d62d7bd0f
psizonenko <psizonenko@griddynamics.com>
2022-05-10 18:14:43 +0300

    small updates

--- app/src/main/java/com/balance/tracker/ui/PurchaseComponents.kt
+++ app/src/main/java/com/balance/tracker/ui/PurchaseComponents.kt
@@ -1,44 +1,86 @@
 package com.balance.tracker.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.*
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
 fun PurchaseScreen() {
-    Column {
-        EnterInfo()
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+    )
+    {
+        EnterAmount()
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        EnterName()
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        EnterPurchaseItem()
     }
 }
 
 @Composable
-fun EnterInfo() {
+fun EnterAmount() {
+    var text by rememberSaveable { mutableStateOf("") }
+    TextField(
+        value = text,
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
+        onValueChange = {
+            text = it
+
+        },
+        label = { Text("Amount") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
+
+//TODO: how to add email or smth
+@Composable
+fun EnterName() {
     var text by rememberSaveable { mutableStateOf("") }
     TextField(
         value = text,
         onValueChange = {
             text = it
         },
-        label = { Text("Amount") }
+        label = { Text("Name") }
     )
 }
+
+@Composable
+fun EnterPurchaseItem() {
+    var text by rememberSaveable { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text("Item") }
+    )
+}
+
 
 
 
